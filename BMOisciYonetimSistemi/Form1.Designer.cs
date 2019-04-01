5a454bffe13e7fd71eb254b9be146f7d0e2b3327
hakanbakacak <hakanbakacak98@gmail.com>
2019-04-01 11:08:25 +0300

    dosya ve resim yukleme eklendi. Kisi eklemek icin ilk adimlar atildi.

--- BMOisciYonetimSistemi/Form1.Designer.cs
+++ BMOisciYonetimSistemi/Form1.Designer.cs
@@ -29,25 +29,30 @@
         private void InitializeComponent()
         {
             this.lbxIsci = new System.Windows.Forms.ListBox();
-            this.btnEkleI = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
             this.tbIsim = new System.Windows.Forms.TextBox();
             this.tbSoyad = new System.Windows.Forms.TextBox();
             this.tbAdres = new System.Windows.Forms.TextBox();
             this.tbMaas = new System.Windows.Forms.TextBox();
-            this.cmbSehirler = new System.Windows.Forms.ComboBox();
-            this.cmbDil = new System.Windows.Forms.ComboBox();
-            this.cbxYoneticilik = new System.Windows.Forms.ComboBox();
-            this.cbxAile = new System.Windows.Forms.ComboBox();
+            this.cmbxSehirler = new System.Windows.Forms.ComboBox();
+            this.cmbxDil = new System.Windows.Forms.ComboBox();
+            this.cmbxYoneticilik = new System.Windows.Forms.ComboBox();
+            this.cmbxAile = new System.Windows.Forms.ComboBox();
             this.grbxAkademik = new System.Windows.Forms.GroupBox();
-            this.cbYuksekL = new System.Windows.Forms.CheckBox();
-            this.cbDoktora = new System.Windows.Forms.CheckBox();
-            this.cbDocent = new System.Windows.Forms.CheckBox();
-            this.cbYuksekLN = new System.Windows.Forms.CheckBox();
             this.cbDrDocN = new System.Windows.Forms.CheckBox();
+            this.cbYuksekLN = new System.Windows.Forms.CheckBox();
+            this.cbDocent = new System.Windows.Forms.CheckBox();
+            this.cbDoktora = new System.Windows.Forms.CheckBox();
+            this.cbYuksekL = new System.Windows.Forms.CheckBox();
             this.dgvListe = new System.Windows.Forms.DataGridView();
             this.IsimSoyisim = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.picBxFoto = new System.Windows.Forms.PictureBox();
+            this.btnFoto = new System.Windows.Forms.Button();
             this.grbxAkademik.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBxFoto)).BeginInit();
             this.SuspendLayout();
             // 
             // lbxIsci
@@ -59,22 +64,23 @@
             this.lbxIsci.Size = new System.Drawing.Size(405, 148);
             this.lbxIsci.TabIndex = 0;
             this.lbxIsci.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.lbxIsci.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbxIsci_MouseDoubleClick);
             // 
-            // btnEkleI
+            // btnEkle
             // 
-            this.btnEkleI.Location = new System.Drawing.Point(305, 166);
-            this.btnEkleI.Name = "btnEkleI";
-            this.btnEkleI.Size = new System.Drawing.Size(112, 32);
-            this.btnEkleI.TabIndex = 1;
-            this.btnEkleI.Text = "Calisan Ekle";
-            this.btnEkleI.UseVisualStyleBackColor = true;
-            this.btnEkleI.Click += new System.EventHandler(this.button1_Click);
+            this.btnEkle.Location = new System.Drawing.Point(271, 166);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(112, 32);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Yeni Çalışan";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.button1_Click);
             // 
             // tbIsim
             // 
             this.tbIsim.Location = new System.Drawing.Point(522, 12);
             this.tbIsim.Name = "tbIsim";
-            this.tbIsim.Size = new System.Drawing.Size(151, 22);
+            this.tbIsim.Size = new System.Drawing.Size(191, 22);
             this.tbIsim.TabIndex = 2;
             this.tbIsim.Text = "İsim";
             // 
@@ -82,7 +88,7 @@
             // 
             this.tbSoyad.Location = new System.Drawing.Point(522, 41);
             this.tbSoyad.Name = "tbSoyad";
-            this.tbSoyad.Size = new System.Drawing.Size(151, 22);
+            this.tbSoyad.Size = new System.Drawing.Size(191, 22);
             this.tbSoyad.TabIndex = 3;
             this.tbSoyad.Text = "Soyisim";
             // 
@@ -91,7 +97,7 @@
             this.tbAdres.Location = new System.Drawing.Point(522, 100);
             this.tbAdres.Multiline = true;
             this.tbAdres.Name = "tbAdres";
-            this.tbAdres.Size = new System.Drawing.Size(151, 75);
+            this.tbAdres.Size = new System.Drawing.Size(191, 75);
             this.tbAdres.TabIndex = 4;
             this.tbAdres.Text = "Adres";
             // 
@@ -99,51 +105,57 @@
             // 
             this.tbMaas.Location = new System.Drawing.Point(522, 181);
             this.tbMaas.Name = "tbMaas";
-            this.tbMaas.Size = new System.Drawing.Size(151, 22);
+            this.tbMaas.Size = new System.Drawing.Size(191, 22);
             this.tbMaas.TabIndex = 5;
             this.tbMaas.Text = "Maaş";
             this.tbMaas.TextChanged += new System.EventHandler(this.tbMaas_TextChanged);
             // 
-            // cmbSehirler
+            // cmbxSehirler
             // 
-            this.cmbSehirler.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.cmbSehirler.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.cmbSehirler.FormattingEnabled = true;
-            this.cmbSehirler.Items.AddRange(new object[] {
+            this.cmbxSehirler.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbxSehirler.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbxSehirler.FormattingEnabled = true;
+            this.cmbxSehirler.Items.AddRange(new object[] {
             "Adana",
             "Ankara"});
-            this.cmbSehirler.Location = new System.Drawing.Point(522, 70);
-            this.cmbSehirler.Name = "cmbSehirler";
-            this.cmbSehirler.Size = new System.Drawing.Size(151, 24);
-            this.cmbSehirler.TabIndex = 6;
-            this.cmbSehirler.Text = "Yaşanılan İl";
+            this.cmbxSehirler.Location = new System.Drawing.Point(522, 70);
+            this.cmbxSehirler.Name = "cmbxSehirler";
+            this.cmbxSehirler.Size = new System.Drawing.Size(191, 24);
+            this.cmbxSehirler.TabIndex = 6;
+            this.cmbxSehirler.Text = "Yaşanılan İl";
             // 
-            // cmbDil
+            // cmbxDil
             // 
-            this.cmbDil.FormattingEnabled = true;
-            this.cmbDil.Location = new System.Drawing.Point(522, 377);
-            this.cmbDil.Name = "cmbDil";
-            this.cmbDil.Size = new System.Drawing.Size(151, 24);
-            this.cmbDil.TabIndex = 8;
-            this.cmbDil.Text = "Yabancı Dil";
+            this.cmbxDil.FormattingEnabled = true;
+            this.cmbxDil.Items.AddRange(new object[] {
+            "a"});
+            this.cmbxDil.Location = new System.Drawing.Point(522, 377);
+            this.cmbxDil.Name = "cmbxDil";
+            this.cmbxDil.Size = new System.Drawing.Size(191, 24);
+            this.cmbxDil.TabIndex = 8;
+            this.cmbxDil.Text = "Yabancı Dil";
             // 
-            // cbxYoneticilik
+            // cmbxYoneticilik
             // 
-            this.cbxYoneticilik.FormattingEnabled = true;
-            this.cbxYoneticilik.Location = new System.Drawing.Point(522, 408);
-            this.cbxYoneticilik.Name = "cbxYoneticilik";
-            this.cbxYoneticilik.Size = new System.Drawing.Size(151, 24);
-            this.cbxYoneticilik.TabIndex = 9;
-            this.cbxYoneticilik.Text = "Yöneticilik Görevi";
+            this.cmbxYoneticilik.FormattingEnabled = true;
+            this.cmbxYoneticilik.Items.AddRange(new object[] {
+            "a"});
+            this.cmbxYoneticilik.Location = new System.Drawing.Point(522, 408);
+            this.cmbxYoneticilik.Name = "cmbxYoneticilik";
+            this.cmbxYoneticilik.Size = new System.Drawing.Size(191, 24);
+            this.cmbxYoneticilik.TabIndex = 9;
+            this.cmbxYoneticilik.Text = "Yöneticilik Görevi";
             // 
-            // cbxAile
+            // cmbxAile
             // 
-            this.cbxAile.FormattingEnabled = true;
-            this.cbxAile.Location = new System.Drawing.Point(522, 439);
-            this.cbxAile.Name = "cbxAile";
-            this.cbxAile.Size = new System.Drawing.Size(121, 24);
-            this.cbxAile.TabIndex = 10;
-            this.cbxAile.Text = "Aile Durumu";
+            this.cmbxAile.FormattingEnabled = true;
+            this.cmbxAile.Items.AddRange(new object[] {
+            "a"});
+            this.cmbxAile.Location = new System.Drawing.Point(522, 439);
+            this.cmbxAile.Name = "cmbxAile";
+            this.cmbxAile.Size = new System.Drawing.Size(191, 24);
+            this.cmbxAile.TabIndex = 10;
+            this.cmbxAile.Text = "Aile Durumu";
             // 
             // grbxAkademik
             // 
@@ -159,35 +171,15 @@
             this.grbxAkademik.TabStop = false;
             this.grbxAkademik.Text = "Akademik Derece";
             // 
-            // cbYuksekL
+            // cbDrDocN
             // 
-            this.cbYuksekL.AutoSize = true;
-            this.cbYuksekL.Location = new System.Drawing.Point(7, 22);
-            this.cbYuksekL.Name = "cbYuksekL";
-            this.cbYuksekL.Size = new System.Drawing.Size(241, 21);
-            this.cbYuksekL.TabIndex = 0;
-            this.cbYuksekL.Text = "Meslek alanı ile ilgili yüksek lisans";
-            this.cbYuksekL.UseVisualStyleBackColor = true;
-            // 
-            // cbDoktora
-            // 
-            this.cbDoktora.AutoSize = true;
-            this.cbDoktora.Location = new System.Drawing.Point(7, 50);
-            this.cbDoktora.Name = "cbDoktora";
-            this.cbDoktora.Size = new System.Drawing.Size(205, 21);
-            this.cbDoktora.TabIndex = 1;
-            this.cbDoktora.Text = "Meslek alanı ile ilgili doktora";
-            this.cbDoktora.UseVisualStyleBackColor = true;
-            // 
-            // cbDocent
-            // 
-            this.cbDocent.AutoSize = true;
-            this.cbDocent.Location = new System.Drawing.Point(7, 78);
-            this.cbDocent.Name = "cbDocent";
-            this.cbDocent.Size = new System.Drawing.Size(213, 21);
-            this.cbDocent.TabIndex = 2;
-            this.cbDocent.Text = "Meslek alanı ile ilgili doçentlik";
-            this.cbDocent.UseVisualStyleBackColor = true;
+            this.cbDrDocN.AutoSize = true;
+            this.cbDrDocN.Location = new System.Drawing.Point(7, 134);
+            this.cbDrDocN.Name = "cbDrDocN";
+            this.cbDrDocN.Size = new System.Drawing.Size(326, 21);
+            this.cbDrDocN.TabIndex = 4;
+            this.cbDrDocN.Text = "Meslek alanı ile ilgili olmayan doktora/doçentlik ";
+            this.cbDrDocN.UseVisualStyleBackColor = true;
             // 
             // cbYuksekLN
             // 
@@ -199,15 +191,35 @@
             this.cbYuksekLN.Text = "Meslek alanı ile ilgili olmayan yüksek lisans";
             this.cbYuksekLN.UseVisualStyleBackColor = true;
             // 
-            // cbDrDocN
+            // cbDocent
             // 
-            this.cbDrDocN.AutoSize = true;
-            this.cbDrDocN.Location = new System.Drawing.Point(7, 134);
-            this.cbDrDocN.Name = "cbDrDocN";
-            this.cbDrDocN.Size = new System.Drawing.Size(326, 21);
-            this.cbDrDocN.TabIndex = 4;
-            this.cbDrDocN.Text = "Meslek alanı ile ilgili olmayan doktora/doçentlik ";
-            this.cbDrDocN.UseVisualStyleBackColor = true;
+            this.cbDocent.AutoSize = true;
+            this.cbDocent.Location = new System.Drawing.Point(7, 78);
+            this.cbDocent.Name = "cbDocent";
+            this.cbDocent.Size = new System.Drawing.Size(213, 21);
+            this.cbDocent.TabIndex = 2;
+            this.cbDocent.Text = "Meslek alanı ile ilgili doçentlik";
+            this.cbDocent.UseVisualStyleBackColor = true;
+            // 
+            // cbDoktora
+            // 
+            this.cbDoktora.AutoSize = true;
+            this.cbDoktora.Location = new System.Drawing.Point(7, 50);
+            this.cbDoktora.Name = "cbDoktora";
+            this.cbDoktora.Size = new System.Drawing.Size(205, 21);
+            this.cbDoktora.TabIndex = 1;
+            this.cbDoktora.Text = "Meslek alanı ile ilgili doktora";
+            this.cbDoktora.UseVisualStyleBackColor = true;
+            // 
+            // cbYuksekL
+            // 
+            this.cbYuksekL.AutoSize = true;
+            this.cbYuksekL.Location = new System.Drawing.Point(7, 22);
+            this.cbYuksekL.Name = "cbYuksekL";
+            this.cbYuksekL.Size = new System.Drawing.Size(241, 21);
+            this.cbYuksekL.TabIndex = 0;
+            this.cbYuksekL.Text = "Meslek alanı ile ilgili yüksek lisans";
+            this.cbYuksekL.UseVisualStyleBackColor = true;
             // 
             // dgvListe
             // 
@@ -226,22 +238,61 @@
             this.IsimSoyisim.HeaderText = "Ad Soyad";
             this.IsimSoyisim.Name = "IsimSoyisim";
             // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(35, 166);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(112, 32);
+            this.btnGuncelle.TabIndex = 14;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(153, 166);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(112, 32);
+            this.btnSil.TabIndex = 15;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // picBxFoto
+            // 
+            this.picBxFoto.Location = new System.Drawing.Point(735, 12);
+            this.picBxFoto.Name = "picBxFoto";
+            this.picBxFoto.Size = new System.Drawing.Size(127, 148);
+            this.picBxFoto.TabIndex = 16;
+            this.picBxFoto.TabStop = false;
+            // 
+            // btnFoto
+            // 
+            this.btnFoto.Location = new System.Drawing.Point(735, 167);
+            this.btnFoto.Name = "btnFoto";
+            this.btnFoto.Size = new System.Drawing.Size(126, 31);
+            this.btnFoto.TabIndex = 17;
+            this.btnFoto.Text = "Fotoğraf Yükle";
+            this.btnFoto.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(874, 480);
+            this.Controls.Add(this.btnFoto);
+            this.Controls.Add(this.picBxFoto);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
             this.Controls.Add(this.dgvListe);
             this.Controls.Add(this.grbxAkademik);
-            this.Controls.Add(this.cbxAile);
-            this.Controls.Add(this.cbxYoneticilik);
-            this.Controls.Add(this.cmbDil);
-            this.Controls.Add(this.cmbSehirler);
+            this.Controls.Add(this.cmbxAile);
+            this.Controls.Add(this.cmbxYoneticilik);
+            this.Controls.Add(this.cmbxDil);
+            this.Controls.Add(this.cmbxSehirler);
             this.Controls.Add(this.tbMaas);
             this.Controls.Add(this.tbAdres);
             this.Controls.Add(this.tbSoyad);
             this.Controls.Add(this.tbIsim);
-            this.Controls.Add(this.btnEkleI);
+            this.Controls.Add(this.btnEkle);
             this.Controls.Add(this.lbxIsci);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -249,6 +300,7 @@
             this.grbxAkademik.ResumeLayout(false);
             this.grbxAkademik.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBxFoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,15 +309,15 @@
         #endregion
 
         private System.Windows.Forms.ListBox lbxIsci;
-        private System.Windows.Forms.Button btnEkleI;
+        private System.Windows.Forms.Button btnEkle;
         private System.Windows.Forms.TextBox tbIsim;
         private System.Windows.Forms.TextBox tbSoyad;
         private System.Windows.Forms.TextBox tbAdres;
         private System.Windows.Forms.TextBox tbMaas;
-        private System.Windows.Forms.ComboBox cmbSehirler;
-        private System.Windows.Forms.ComboBox cmbDil;
-        private System.Windows.Forms.ComboBox cbxYoneticilik;
-        private System.Windows.Forms.ComboBox cbxAile;
+        private System.Windows.Forms.ComboBox cmbxSehirler;
+        private System.Windows.Forms.ComboBox cmbxDil;
+        private System.Windows.Forms.ComboBox cmbxYoneticilik;
+        private System.Windows.Forms.ComboBox cmbxAile;
         private System.Windows.Forms.GroupBox grbxAkademik;
         private System.Windows.Forms.CheckBox cbDocent;
         private System.Windows.Forms.CheckBox cbDoktora;
@@ -274,6 +326,10 @@
         private System.Windows.Forms.CheckBox cbYuksekLN;
         private System.Windows.Forms.DataGridView dgvListe;
         private System.Windows.Forms.DataGridViewTextBoxColumn IsimSoyisim;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.PictureBox picBxFoto;
+        private System.Windows.Forms.Button btnFoto;
     }
 }
 
--- BMOisciYonetimSistemi/Form1.cs
+++ BMOisciYonetimSistemi/Form1.cs
@@ -22,35 +22,61 @@ namespace BMOisciYonetimSistemi
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Id,Name,Surname,address,salary
+            // AŞAĞIDA YAPILAN İŞLEM İÇİN BİR SINIF OLUŞTURULUP BURADAKİ KALABALIK TEMİZLENMESİ GEREKİYOR.
+            //https://sanalkurs.net/c-openfiledialog-kullanimi-10552.html
+            OpenFileDialog fdSource = new OpenFileDialog();
+            fdSource.Filter = "kaynak |*.tsv| kaynak |*.csv"; // burada iki adet filtre verdik.
+            fdSource.FilterIndex = 2;// bu kod ile varsayılan olarak 2. filtre ile açılacaktır.
+            fdSource.ShowDialog();// openfiledialog ekranını açar
+            fdSource.CheckFileExists = false;// bu kod dosya adı kısmına bir isim yazdığınızda dosya var mı yok mu kontrolünü yapar. varsa altta öneri çıkar.
+                                             //fdSource.Title = "Lütfen Bir Kaynak Dosyası Seçiniz."; //Pencerenin üstünde çıkması gerekiyor. Fakat yok ??
+            string DosyaYolu="Deneme.csv";//varsayılan dosya
+            DosyaYolu = fdSource.FileName;
+            //Dosya yolu seçilmediğinde hata veriyor. dosya yolu seçilmediğinde filedialog kapanmamalı.
+
+            // AŞAĞIDA YAPILAN İŞLEM İÇİN BİR SINIF OLUŞTURULUP BURADAKİ KALABALIK TEMİZLENMESİ GEREKİYOR.
             string line;
-            FileStream file = new FileStream("Deneme.txt",FileMode.Open);
+            FileStream file = new FileStream(DosyaYolu,FileMode.Open);
             Encoding tr = Encoding.GetEncoding("iso-8859-9");
             StreamReader sr = new StreamReader(file, tr);
-            
+
+            char split;
+            if (DosyaYolu.Split('.').Last() == "csv")
+            {
+                split = ',';
+            }
+            else
+            {
+                split = '\t';
+            }
             while ((line = sr.ReadLine()) != null)
             {
-                string[] bilgiler = line.Split(',');
-                //lbxIsci.Items.Add(line);
+                string[] bilgiler = line.Split(split);//burada dosya formatı csv ise ',' tsv ise '\t' koşulu eklenecek.
                 lbxIsci.Items.Add(bilgiler[0] +'\t'+ bilgiler[1] + '\t' + bilgiler[2] + '\t' + bilgiler[3] + '\t' + bilgiler[4]);
                 calisanlar.Add(new Calisan(bilgiler[0], bilgiler[1], bilgiler[2], bilgiler[3], Convert.ToDouble(bilgiler[4])));
-                //lbxIsci.Items.Add(bilgiler[0] + '\t' + bilgiler[1] + '\t' + bilgiler[2] + '\t' + bilgiler[3] + '\t' + bilgiler[4]);
-
             }
-
             file.Close();
+             
 
-            
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            if ((tbIsim.Text == "İsim") || (tbSoyad.Text == "Soyisim") || (tbAdres.Text == "Adres") || (tbMaas.Text == "Maaş")||
+                (cmbxAile.SelectedIndex==-1)||(cmbxDil.SelectedIndex==-1)||(cmbxSehirler.SelectedIndex==-1)||
+                (cmbxYoneticilik.SelectedIndex==-1))
+            {
+                MessageBox.Show("Lütfen Tüm Boşlukları Doldurunuz.");
+            }
+            else
+            {
+
+            }
         }
 
         private void tbMaas_TextChanged(object sender, EventArgs e)
@@ -58,6 +84,10 @@ namespace BMOisciYonetimSistemi
 
         }
 
-   
+        private void lbxIsci_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Form2 FormDetay = new Form2();
+            FormDetay.Show();
+        }
     }
 }
